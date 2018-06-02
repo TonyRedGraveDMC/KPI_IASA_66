9b3b6a9ddb366260a97c4614095c316c31c6ce70
Alex <eskander8@gmail.com>
2018-06-02 21:47:32 +0600

    Cnumbers & split

---
+++ Cnumber.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static Kyrsach2018.Program;
+
+namespace Kyrsach2018
+{
+    class Cnumber : AbstractNumber<Cnumber>
+    {
+        private double rPart, imPart;
+
+       
+        public Cnumber(double rpart, double impart)
+        {
+            this.rPart = rpart;
+            this.imPart = impart;
+        }
+
+        public double Impart
+        {
+            get
+            {
+                return imPart;
+            }
+
+            set
+            {
+                imPart = value;
+            }
+        }
+
+        public double Rpart
+        {
+            get
+            {
+                return rPart;
+            }
+
+            set
+            {
+                rPart = value;
+            }
+        }
+
+        public override Cnumber Add(Cnumber Complex)
+        {
+            return new Cnumber(rPart + Complex.Rpart, imPart + Complex.Impart);
+        }
+
+        public override Cnumber Division(Cnumber t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Cnumber Minus(Cnumber t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Cnumber Multiply(Cnumber t)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
-namespace Kyrsach2k18
+namespace Kyrsach2018
 {
-    class Program
+    public class Program
     {
         //abstract class AbstractNumber
         //{
@@ -16,7 +17,7 @@ namespace Kyrsach2k18
         //class
 
 
-        abstract class Number<T>
+       public abstract class AbstractNumber<T>
         {
             private double num;
 
@@ -44,7 +45,7 @@ namespace Kyrsach2k18
             }
         }
 
-        class Rnumber : Number<Rnumber>
+        class Rnumber : AbstractNumber<Rnumber>
         {
             public Rnumber(double num)
             {
@@ -78,21 +79,36 @@ namespace Kyrsach2k18
             }
         }
 
+       
+
         static void Main(string[] args)
         {
+            string iN = "3324 - 3414i";
+            Console.WriteLine(iN);
 
-            Rnumber r1 = new Rnumber(320);
-            Rnumber r2 = new Rnumber(0);
-            Rnumber r3 = r1.Add(r2);
-            Rnumber r4 = r1.Minus(r2);
-            Rnumber r5 = r1.Multiply(r2);
-            Rnumber r6 = r1.Division(r2);
+            iN = iN.Replace("i", "");
+            Console.WriteLine(iN);
+
+            string pattern = @"\+|\-";
+            string[] tokens = Regex.Split(iN, pattern);
+            string sign = Regex.Match(iN, pattern).Value;
+            double rPart = Convert.ToDouble(tokens[0]);
+            double imPart = Convert.ToDouble(tokens[1]);
+            Console.WriteLine(rPart + sign + imPart + "i");
+        
+
+            //Rnumber r1 = new Rnumber(320);
+            //Rnumber r2 = new Rnumber(0);
+            //Rnumber r3 = r1.Add(r2);
+            //Rnumber r4 = r1.Minus(r2);
+            //Rnumber r5 = r1.Multiply(r2);
+            //Rnumber r6 = r1.Division(r2);
 
 
-            Console.WriteLine(r3);
-            Console.WriteLine(r4);
-            Console.WriteLine(r5);
-            Console.WriteLine(r6);
+            //Console.WriteLine(r3);
+            //Console.WriteLine(r4);
+            //Console.WriteLine(r5);
+            //Console.WriteLine(r6);
 
 
 
